170f3ae09920ba9183c54c6baaa6ce7f9da07b13
Bradigson <71954800+Bradigson@users.noreply.github.com>
2022-01-18 13:30:42 -0400

    Tercer Cambio Agregado (ToTal)

--- js/app.js
+++ js/app.js
@@ -14,6 +14,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
 cards.addEventListener("click", e=>{
     addCarrito(e);
 })
+
+items.addEventListener("click",(e)=>{
+    btnAccion(e);
+})
 const getData = async()=>{
     try{
 
@@ -78,5 +82,70 @@ const setCarrito = objeto =>{
 
 const pintarCarito = ()=>{
 
-    console.log(carrito)
+    console.log(carrito);
+
+    items.innerHTML = ''
+    Object.values(carrito).forEach(producto=>{
+        templateCarrito.querySelector('th').textContent = producto.id
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.title
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+        templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
+        const clone = templateCarrito.cloneNode(true)
+        Fragment.appendChild(clone)
+    })
+    items.appendChild(Fragment);
+
+    pintarFooter();
+
+}
+
+const pintarFooter =()=>{
+    footer.innerHTML = '';
+    if(Object.keys(carrito).length === 0){
+        footer.innerHTML = `
+        <th scope="row" colspan="5">Carrito vacio - comience a comprar!</th>
+        `
+        return
+    }
+    const nCantidad = Object.values(carrito).reduce((acc, {cantidad})=> acc + cantidad, 0)
+    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio})=> acc + cantidad * precio, 0)
+    //console.log(nPrecio);
+
+    templateFooter.querySelectorAll('td')[0].textContent = nCantidad;
+    templateFooter.querySelector('span').textContent = nPrecio;
+
+    const clone = templateFooter.cloneNode(true);
+    Fragment.appendChild(clone);
+    footer.appendChild(Fragment)
+
+    const btnVaciar = document.querySelector('#vaciar-carrito');
+    btnVaciar.addEventListener("click",()=>{
+        carrito = {}
+        pintarCarito();
+        
+    })
+}
+
+const btnAccion =  e=>{
+    
+    if(e.target.classList.contains('btn-info')){
+        console.log(carrito[e.target.dataset.id])
+
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad = carrito[e.target.dataset.id].cantidad + 1;
+        carrito[e.target.dataset.id] = {...producto}
+        pintarCarito();
+    }
+    if(e.target.classList.contains('btn-danger')){
+        const producto = carrito[e.target.dataset.id]
+         producto.cantidad--
+        if(producto.cantidad === 0){
+            delete carrito[e.target.dataset.id]
+        }
+        pintarCarito()
+    }
+
+    e.stopPropagation()
 }
